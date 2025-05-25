b1342b0a99f694a87f2332d84b7095411e601301
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-05-25 20:31:58 +0530

    modified:src/Pages/Anatomy.jsx, modified:src/Styles/Anatomy.css

--- src/Pages/Anatomy.jsx
+++ src/Pages/Anatomy.jsx
@@ -13,7 +13,23 @@ function Anatomy() {
     return (
         <div className="Anatomy_MainDiv">
             <div className='Anatomy_Status'>
-                <img src={anatomyImage} alt="Anatomical Illustration" className="Anatomy_Image" />
+                {/* <img src={anatomyImage} alt="Anatomical Illustration" className="Anatomy_Image" /> */}
+
+                <div className="Body_Parts">
+                    <img src={anatomyImage} alt="Anatomy" className="Anatomy_Image" />
+
+                    <div className="label" style={{ top: "90px", left: "140px" }}>
+                        <div className="Highlight_Box"></div>
+                        <span style={{ background: "darkblue", color: "white" }} >❤️ Health Heart</span>
+                    </div>
+
+                    <div className="label" style={{ top: "290px", left: "-16px" }}>
+                        <span style={{ background: "cyan", color: "black" }}>🦵 Healthy Leg</span>
+                        <div className="Highlight_Box"></div>
+                    </div>
+
+                </div>
+
 
                 <div className="Org_Details">
                     <HealthStatusCards Org_IMG={Lungs} Org_Name={"Lungs"} Apo_Date={"24-May-2025"} Color={"Darkred"} Status={60} />
@@ -21,6 +37,11 @@ function Anatomy() {
                     <HealthStatusCards Org_IMG={Teeth} Org_Name={"Teeth"} Apo_Date={"25-May-2025"} Color={"Green"} Status={60} />
 
                     <HealthStatusCards Org_IMG={Bone} Org_Name={"Bone"} Apo_Date={"20-May-2025"} Color={"DarkOrange"} Status={60} />
+
+                    <div style={{
+                        alignSelf: "end",
+                        color: 'grey'
+                    }}>Details 🡢</div>
                 </div>
             </div>
 
@@ -30,4 +51,3 @@ function Anatomy() {
 };
 
 export default Anatomy;
-
