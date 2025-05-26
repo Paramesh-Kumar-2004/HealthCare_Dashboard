5fd6abdcd1f9dc3c5b1a068e26a4ac9c31452f1d
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-05-26 10:18:10 +0530

    modified:src/App.js,  modified:src/Styles/App.css,  modified:src/index.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react';
 import DashboardMainContent from './Pages/DashboardMainContent';
 import SideBar from './Components/SideBar';
 import Header from './Components/Header';
@@ -5,16 +6,23 @@ import './Styles/App.css';
 
 
 function App() {
+
+  useEffect(() => {
+    alert("Dektop View Only (Mobile View Not Complete)");
+  }, []);
+
   return (
-    <div className="App_MainDiv">
-      <div className='Side_Bar'>
-        <SideBar />
+    <>
+      <div className="App_MainDiv">
+        <div className='Side_Bar'>
+          <SideBar />
+        </div>
+        <div className="Main_Content">
+          <Header />
+          <DashboardMainContent />
+        </div>
       </div>
-      <div className="Main_Content">
-        <Header />
-        <DashboardMainContent />
-      </div>
-    </div>
+    </>
   );
 }
 
